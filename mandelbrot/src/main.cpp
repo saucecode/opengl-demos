c6b1f65add2f2ba472f2d5ad03f388dbe1930958
Julian <cahill.julian@gmail.com>
2019-02-04 06:17:49 +1300

    add a sleep, for stability

--- mandelbrot/src/main.cpp
+++ mandelbrot/src/main.cpp
@@ -192,6 +192,8 @@ int main( int argc, char *argv[] ) {
 		update(dt);
 		currentTime = glfwGetTime();
 
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
